1b18dc8666032d878401ee6760fc18272c7e7eb3
Braulio Hernández <72417322+BraulioHzGz@users.noreply.github.com>
2023-03-01 18:29:40 -0600

    Aporte de Braulio Hernández
    Solo por ocio.

---
+++ Recomendación Semanal/Recomendación.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    char dia[10];
+    printf("\nDía en que consultas esto:  ");
+    scanf("%s", dia);
+    printf("\nQue tengas buen %s\nRecuerda acabar tu tarea!", dia);
+    return 0;
+}
+
+//La recomendación del día en C
